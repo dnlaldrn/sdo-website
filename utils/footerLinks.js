223bb58635b908a37bf38f3ab575f4230de6032c
kuroi017 <painkira518@gmail.com>
2026-06-23 18:42:25 +0800

    refactor(data): modularize UI data arrays into utility files

---
+++ utils/footerLinks.js
@@ -0,0 +1,7 @@
+export const footerLinks = [
+  { name: "Privacy Policy", href: "#" },
+  { name: "Terms of Service", href: "#" },
+  { name: "SDG Portal", href: "#" },
+  { name: "University Site", href: "#" },
+  { name: "Contact Us", href: "#" },
+];
